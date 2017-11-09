c191bbb17443fd5cf07b2c990802275b33d13e6f
Tremaun <mtremaun@gmail.com>
2017-11-09 16:50:47 -0700

    Fixing

---
+++ NimTheGame/NimTheGame/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimTheGame
+{
+    public class Player
+    {
+        private bool isHuman;
+        private bool isTurn;
+        private String name;
+
+        public Player(String name, bool isHuman, bool isTurn)
+        {
+            this.name = name;
+            this.isHuman = isHuman;
+            this.isTurn = isTurn;
+        }
+
+        public getName
+    }
+}
