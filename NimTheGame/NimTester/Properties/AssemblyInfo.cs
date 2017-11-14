fa634dbbb6fd272716a72dea64df7350c12c7a55
Elena Johnson <EJohnson@student.neumont.edu>
2017-11-14 10:29:55 -0700

    Started Testing
    Built the bare bones of the tester project. Assigned regions for each persons testing

---
+++ NimTheGame/NimTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NimTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NimTester")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("362e1e4c-7093-40cb-8a99-4d253c03da0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NimTheGame/NimTester/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NimTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        #region MenuTests
+
+        //Elena
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        #endregion
+
+        #region BoardTests
+
+        //Elena
+
+        #endregion
+
+        #region GameTests
+
+        //Austin
+
+        #endregion
+
+        #region DriverTests
+
+        //Austin
+        //I'm not sure if we need this but just in case
+
+        #endregion
+
+        #region PlayerTests
+
+        //Tre
+
+        #endregion
+
+        #region HeapTests
+
+        //Tre
+
+        #endregion
+
+
+    }
+}
