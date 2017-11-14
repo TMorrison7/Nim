7bfe53df586eae215d40c36fe65f7844eb1040fc
Elena Johnson <EJohnson@student.neumont.edu>
2017-11-14 12:52:16 -0700

    UnitTestingProgress

--- NimTheGame/NimTester/UnitTest1.cs
+++ NimTheGame/NimTester/UnitTest1.cs
@@ -7,19 +7,145 @@ namespace NimTester
     public class UnitTest1
     {
         #region MenuTests
-
         //Elena
+        #region PromptForInt Tests
+        [TestMethod]
+        public void PromptForInt_AcceptsMinimumAsValidInput()
+        {
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        public void PromptForInt_AccepstMaximumAsValidInput()
         {
+
+        }
+        [TestMethod]
+        public void PromptForInt_AcceptsBetweenMaxAndMinAsValidInput()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForInt_HandlesInputLessThanMinCorrectly()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForInt_HandlesInputGreaterThanMaxCorrectly()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForInt_HandlesEmptyInput()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForInt_HandlesNullInput()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForInt_DisplaysOptions()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForInt_DisplaysPromptAfterBadInput()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForInt_HandlesAlphaCharacters()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForInt_HandlesFloatingPointValues()
+        {
+
         }
         #endregion
 
-        #region BoardTests
+        #region PromptForNameTests
+        [TestMethod]
+        public void PromptForName_DisplaysPrompt()
+        {
 
+        }
+        [TestMethod]
+        public void PromptForName_AcceptsValidInput()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForName_HandlesEmptyInput()
+        {
+
+        }
+        [TestMethod]
+        public void PromptForName_HandlesNullInput()
+        {
+
+        }
+        #endregion
+
+        #endregion
+
+        #region BoardTests
         //Elena
 
+        #region AddHeap Tests
+        [TestMethod]
+        public void AddHeap_AcceptsAValidHeap()
+        {
+
+        }
+        //out of ideas
+        #endregion
+
+        #region BoardCtor Tests
+        [TestMethod]
+        public void Board_ClearsHeaps()
+        {
+
+        }
+        #endregion
+
+        #region CheckWin Tests
+        [TestMethod]
+        public void CheckWin_idk()
+        {
+
+        }
+        //idk what to put here
+        #endregion
+
+        #region getHeapSize Tests
+        [TestMethod]
+        public void getHeapSize_idk()
+        {
+
+        }
+        //idk
+        #endregion
+
+        #region getMaxNumber Tests
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+        //idk
+        #endregion
+
+        #region removeMatchesFrom Tests
+        [TestMethod]
+        public void r()
+        {
+
+        }
+        #endregion
+        
         #endregion
 
         #region GameTests
